0eb1b28a22b60cfe6eeb06f34423a59e4e85312a
phuongtagcd19842 <66770565+phuongtagcd19842@users.noreply.github.com>
2021-03-01 23:30:46 +0700

    shop2.php

---
+++ shop2.php
@@ -0,0 +1,79 @@
+<html>
+	<head>
+    <link rel="stylesheet" href ="styles.css">
+    <title> Database </title>
+    <marquee bgcolor="gray" 
+             scrollamount="12" 
+             direction="left" 
+             onmouseover="this.stop();" 
+             onmouseout="this.start();">
+        <center>
+		<?php
+		$date = getdate();
+			echo "<hr>";
+			echo "Today is: ".$date['weekday']."--".$date['mday']."/".$date['mon']."/".$date['year']."--".$date['hours'].":".$date['minutes'].":".$date['seconds'];
+		?>
+	  </center>
+    </marquee>
+    <ul>
+        <li> <a href="">VIEW DATABASE</a> </li>
+	    <li> <a href="login.php">LOG OUT</a> </li>
+    </ul>
+  </head>
+    <body>
+      <style>
+        body {
+          background-image: url('background.jpg');
+          background-attachment: fixed;
+          background-size: 100%100%;
+        } 
+      </style>
+	    <table border="2">
+		    <tr>
+			    <th>Product ID</th>
+			    <th>Product Name</th>
+			    <th>Product Price</th>
+			    <th>Quantity</th>
+			    <th colspan="2" align="center">Operation</th>
+		    </tr>
+		<?php 
+			echo '<p>ATN Shop </p>'; 
+			$host_heroku = "ec2-54-146-73-98.compute-1.amazonaws.com";
+			$db_heroku = "d13da88n45ouc0";
+			$user_heroku = "vzhpjzlpafmtft";
+			$pw_heroku = "a49e15b38751156392c0251a8e0bc2645d2bb5029a4c15c397b9a3e3a88b81ef";
+			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+			$pg_heroku = pg_connect($conn_string);
+			
+			if (!$pg_heroku)
+			{
+				die('Error: Could not connect: ' . pg_last_error());
+			}
+		    	$query = 'select * from atnshop2';
+			$data = pg_query($pg_heroku, $query);
+	    		$total = pg_num_rows($data);
+			if($total!=0)
+			{
+				while ($result=pg_fetch_assoc($data))
+				{
+					echo "
+					<tr>
+					<td>".$result['productid']."</td>
+					<td>".$result['productname']."</td>
+					<td>".$result['productprice']."</td>
+					<td>".$result['quantityonhand']."</td>
+					<td><a href='update2.php?pi=$result[productid]&pn=$result[productname]&pp=$result[productprice]&qt=$result[quantityonhand]'>
+					Edit/Update</td>
+					<td><a href='delete2.php?pi=$result[productid]'>Delete</td>
+					
+					</tr>
+					";
+				}
+			}
+	    
+		?> 
+		    <form action="https://asm-atn-shop.herokuapp.com/add2.php">
+    				<input type="submit" value="Add" />
+			</form>
+	</body>
+</html>
