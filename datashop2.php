32515f671efb9503439a0acf3710962c88473267
phuongtagcd19842 <66770565+phuongtagcd19842@users.noreply.github.com>
2021-03-01 23:22:08 +0700

    Update datashop2.php

--- datashop2.php
+++ datashop2.php
@@ -23,7 +23,7 @@
 <body>
 	<style>
         body {
-          background-image: url('background2.jpg');
+          background-image: url('background2.png');
           background-attachment: fixed;
           background-size: 100%100%;
         } 
